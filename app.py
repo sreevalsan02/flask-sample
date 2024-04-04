6e9180fdc25b824701c992356707c934ba8e79a4
Sreevalsan <98298661+sreevalsan02@users.noreply.github.com>
2024-04-04 21:39:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-# from ultralytics import RTDETR
-# import cv2
-# import io
-# from PIL import Image
-from flask import Flask, request,send_file
+from flask import Flask, request, send_file
 from flask_cors import CORS
+import io
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -11,48 +9,52 @@ CORS(app)  # Enable CORS for all routes
 
 @app.route('/', methods=['GET'])
 def hello():
-  
-  return "hello you are in root page"
+    return "hello you are in root page"
 
 @app.route('/message')
-
 def message():
-  return "hi message"
+    return "hi message"
 
+
+# Lazy loading for the object detection model
+def get_model():
+    model = None
+    def inner():
+        nonlocal model
+        if model is None:
+            from ultralytics import RTDETR
+            model = RTDETR('best.pt')
+        return model
+    return inner
 
 @app.route('/upload', methods=['GET', 'POST'])
-
 def table_image():
-    if request.method =='POST':
+    if request.method == 'POST':
         img_file = request.files['the_file']
-        
-        cropped_img = process_image(img_file)
+
+        # Use lazy-loaded model
+        model = get_model()()
+
+        img = Image.open(img_file)
+        results = model(img)
+        boxes = results[0].boxes
+        lis = []
+        for i in boxes.xyxy[0]:
+            lis.append(i.item())
+
+        x = int(lis[0])
+        y = int(lis[1])
+        x_end = int(lis[2])
+        y_end = int(lis[3])
+
+        im1 = img.crop((x, y, x_end, y_end))
+
         img_io = io.BytesIO()
-        cropped_img.save(img_io, 'PNG')
+        im1.save(img_io, 'PNG')
         img_io.seek(0)
         return send_file(img_io, mimetype='image/png')
-        
+
     return 'noo'
 
-def process_image(file):
-    model = RTDETR('best.pt')
-    img = Image.open(file)
-    results = model(img)
-    boxes = results[0].boxes
-    lis = []
-    for i in boxes.xyxy[0]:
-        lis.append(i.item())
-       
-
-    x = int(lis[0])
-    y = int(lis[1])
-    x_end = int(lis[2])
-    y_end = int(lis[3])
-
-    im1 = img.crop((x,y,x_end,y_end))
-    
-    return im1
-
-
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
